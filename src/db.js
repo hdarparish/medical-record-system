3919238d5c4f106490b76e759c668f804eae02ca
hdarparish <hosseindarp@gmail.com>
2020-08-30 19:42:51 -0400

    edit db.js

--- src/db.js
+++ src/db.js
@@ -1,4 +1,5 @@
 import mysql from "mysql";
+import util from "util";
 
 const db = mysql.createConnection({
   host: "localhost",
@@ -7,10 +8,33 @@ const db = mysql.createConnection({
   database: "health_System",
 });
 
+const query = util.promisify(db.query).bind(db);
+
 // open the MySQL connection
 db.connect((error) => {
   if (error) throw error;
   console.log("Successfully connected to the database.");
 });
 
-export default db;
+const userLogin = async (reqBody) => {
+  let username = reqBody.username;
+
+  try {
+    const queryResult = await query(
+      `Select * from users where username = '${username}'`
+    );
+    //check if there is a result from the query
+    if (queryResult.length > 0) {
+      return queryResult;
+    }
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const viewList = async (tableName) => {
+  const queryResult = await query(`Select * from ${tableName}`);
+  return queryResult;
+};
+
+export { userLogin, viewList };
