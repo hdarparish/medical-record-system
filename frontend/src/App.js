5cd75d89e2adda41ff04963a7deafd068148740b
hd <hdarparish@gmail.com>
2021-05-14 15:20:51 -0400

    add page components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,20 @@ import Login from "./pages/Login";
 import Home from "./pages/Home";
 //components
 import Footer from "./components/Footer";
+import Nav from "./components/Nav";
+//Router
+import { Switch, Route, BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-      <Home />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Nav />
+        <Login />
+        <Home />
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
